d2dcb5339900a1ad6144999cb55ca231d18aec81
Alex Titarenko <alex.titarenko@talex-soft.com>
2013-01-14 23:37:09 +0200

    TAlex.PADGenerator project added to repository.

---
+++ TAlex.PADGenerator/Models/PADRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+
+namespace TAlex.PADGenerator.Models
+{
+    [XmlRoot(ElementName = "XML_DIZ_INFO")]
+    public class PADRoot
+    {
+    }
+}
---
+++ TAlex.PADGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TAlex.PADGenerator")]
+[assembly: AssemblyDescription("PAD File Generator Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("T-Alex Software")]
+[assembly: AssemblyProduct("TAlex.PADGenerator")]
+[assembly: AssemblyCopyright("Copyright © 2013 T-Alex Software")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc3864cb-aa6e-44f6-9b09-f2646d474c2e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
